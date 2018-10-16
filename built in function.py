ba188aebf659b018d1f9bca53418694acb7238e7
pvgneha <44013712+pvgneha@users.noreply.github.com>
2018-10-16 12:06:40 +0530

    Add files via upload

---
+++ built in function.py
@@ -0,0 +1,39 @@
+Python 2.7.14 (v2.7.14:84471935ed, Sep 16 2017, 20:19:30) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> abs(-45)
+45
+>>> abs(119l)
+119L
+>>> max(20,80,100)
+100
+>>> min(90,80,50)
+50
+>>> divmod(14,5)
+(2, 4)
+>>> divmod(13,5)
+(2, 3)
+>>> a=[1,6,9]
+>>> lent(a)
+
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    lent(a)
+NameError: name 'lent' is not defined
+>>> len(a)
+3
+>>> a=[2,5,7,1,9]
+>>> len(a)
+5
+>>> range(10)
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+>>> range(0,-10,-1)
+[0, -1, -2, -3, -4, -5, -6, -7, -8, -9]
+>>> range(2,10)
+[2, 3, 4, 5, 6, 7, 8, 9]
+>>> b="hello"
+>>> len(b)
+5
+>>> b="hello world"
+>>> len(b)
+11
+>>> 
---
+++ swap.py
@@ -0,0 +1,9 @@
+#python program to swap two variable
+# to take input from the user
+#x=input('enter value of x')
+#y=input('enter value of y')
+#create a temporary variable and swap the values temp=x
+x=y
+y=temp
+print ('the value of x after swapping:()',format(x))
+print('the value of y after swapping:()',format(y))
